4b74a8c2d33af3ee78976ccb03011a31902aa76a
Gregory Gauthier <talonk165@hotmail.com>
2019-01-31 17:59:08 -0500

    This assignment is now on github

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,26 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+
+                
+                Console.WriteLine("Hello User Please Enter your NAME!!");
+
+                string UserInput = null;
+                UserInput = (Console.ReadLine());
+                Console.WriteLine("Hello " + UserInput);
+
+
+            }
+
+            catch
+            {
+                Console.WriteLine("Please TRy another Set Of Values");
+                Console.ReadKey(true);
+            }
+
+
         }
     }
 }
